a721527ed5688cb8af218d924e3b236442e8d7f6
Olle Bergkvist <hello@Ollebergkvist.com>
2022-09-27 13:33:25 -0600

    style: remove comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import image from '@astrojs/image'
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [sitemap(), tailwind(), image()],
 })
